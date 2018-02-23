628d94e983f3b272bb0e10f7a02a32a96aefdbef
Doug Sherman <dsherman@ucdavis.edu>
2018-02-23 10:10:44 -0800

    Another tweak to the gc_storage file to import needed functions explicitly

--- StorageClient/gc_storage_test.py
+++ StorageClient/gc_storage_test.py
@@ -1,9 +1,12 @@
-import gc_storage
+from gc_storage import list_files,get_img,get_report,get_negatives
 
 # Get the list of all blobs (files)
 files = list_files()
 pos_files = [x for x in files if 'diagrams_with_google_label' in x.name]
 neg_files = [x for x in files if 'negatives' in x.name]
+# Note you could also do, but this is about twice as long
+# pos_files = list_files('diagrams_with_google_label/')
+# neg_files = list_files('negatives/')
 
 # Get image and report from a given ID
 report_id = '9120-2015-0083'
