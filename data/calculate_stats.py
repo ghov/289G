f351a35477bacf8766514cb8383d3b0fd7e9473c
Nicholas Joodi <npjoodi@ucdavis.edu>
2018-03-06 10:45:24 -0800

    calculates mean distance of pairs

--- data/calculate_stats.py
+++ data/calculate_stats.py
@@ -5,10 +5,14 @@ train_file = 'train.txt'
 data_directory = '/Users/nicholasjoodi/Documents/ucdavis/computerScience/VisualRecognition/diagrams_with_google_label/'
 default_image_size = 224
 stats_file = 'stats.txt'
+# report_mean = np.array([245.79311224, 245.81652982, 245.81164501])
+# satelite_mean = np.array([ 93.08452348, 109.98226443, 109.82506676])
+# report_std = np.array([39.58706872, 39.50749516, 39.53638409])
+# satelite_std = np.array([44.26688215, 47.45916113, 48.47259675])
 with open(train_file, 'r') as _file:
     rows = _file.readlines()
     size = len(rows)
-    # size = 20
+    size = 20
     np_reports = np.empty((size, default_image_size, default_image_size,3))
     np_satelites = np.empty((size, default_image_size, default_image_size,3))
     for i in range(size):
@@ -31,6 +35,16 @@ with open(train_file, 'r') as _file:
     std_satelite = np.std(np_satelites,(0,1,2))
     print(mean_satelite)
     print(std_satelite)
+    distance = np.empty((size))
+    for i in range(size):
+        img_report = np_reports[i]
+        img_satelite = np_satelites[i]
+        a = (img_report - mean)/ std
+        b = (img_satelite - mean_satelite)/ std_satelite
+        distance[i] = np.linalg.norm(a-b)
+    mean_dist = np.mean(distance)
+    print("mean distance:")
+    print(str(mean_dist))
 with open(stats_file, 'w') as s_file:
     s_file.write("report statistics:\n")
     s_file.write(" - mean:")
@@ -41,4 +55,6 @@ with open(stats_file, 'w') as s_file:
     s_file.write(" - mean:")
     s_file.write(str(mean_satelite)+'\n')
     s_file.write(" - std:")
-    s_file.write(str(std_satelite))
+    s_file.write(str(std_satelite)+'\n')
+    s_file.write("mean distance:\n")
+    s_file.write(str(mean_dist)+'\n')
