de77e6a73f789177ffb52aac2b5d419397a1a3ca
Nicholas Joodi <npjoodi@ucdavis.edu>
2018-03-09 04:01:31 -0800

    added validation in training

--- siamese/train_network.py
+++ siamese/train_network.py
@@ -15,18 +15,23 @@ import os
 import data
 import cv2
 from data_orchestrator import DataOrchestrator
+from test_data_orchestrator import TestDataOrchestrator
+import metrics
 
 from tensorflow.contrib import slim
 image_size = inception.inception_v1.default_image_size
-
+checkpoint_path = 'siamese_checkpoint/'
 model = 'siamese_finetuned/model'
 print("begin tensor session")
 epochs = 50
-batch_size = 6
+batch_size = 1
 save_step = 5
 train_file = '../data/train.txt'
-train_orch = DataOrchestrator(train_file, shuffle = True)
+test_file = '../data/test_with_neg.txt'
+train_orch = DataOrchestrator(train_file, shuffle = True, corruption_size=1)
+test_orch = TestDataOrchestrator(test_file)
 batches_per_epoch = np.floor(train_orch.dataset_size / batch_size).astype(np.int16)
+test_batches_per_epoch = np.floor(test_orch.dataset_size / batch_size).astype(np.int16)
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 with tf.Session(config = config) as sess:
@@ -41,6 +46,8 @@ with tf.Session(config = config) as sess:
     loss =  tf.get_collection('loss')[0]
     report_features = tf.get_collection('report_features')[0]
     satelite_features = tf.get_collection('satelite_features')[0]
+    d = tf.reduce_sum(tf.square(report_features - satelite_features), 1)
+    d_sqrt = tf.sqrt(d)
 
 
     print("Begin training...")
@@ -55,7 +62,34 @@ with tf.Session(config = config) as sess:
                 saver.save(sess,model)
                 print('Saved model parameters')
             step += 1
+            break
+
+
+        print("{} testing".format(datetime.now()))
+        total_labels = None
+        total_logits = None
+        for step in range(test_batches_per_epoch):
+            batch_t_reports, batch_t_satelites, batch_t_labels = test_orch.get_next_testing_batch(batch_size)
+            logits = sess.run(d_sqrt, feed_dict={reports: batch_t_reports, satelites: batch_t_satelites, labels:batch_t_labels, train_phase:False})
+            if step == 0:
+                total_logits = logits
+                total_labels = batch_t_labels
+            else:
+                total_logits = np.concatenate((total_logits, logits), axis=0)
+                total_labels = np.concatenate((total_labels, batch_t_labels), axis=0)
+            break
+        total_labels = total_labels.astype(int)
+        aucPR, average_precision, roc_auc = metrics.get_metrics(total_labels,total_logits)
+        print('aucPR: '+str(aucPR))
+        print('average_precision: '+str(average_precision))
+        print('roc_auc: '+str(roc_auc))
+
+        checkpoint_name = os.path.join(checkpoint_path, 'model_epoch'+str(epoch+1)+'.ckpt')
+        save_path = saver.save(sess, checkpoint_name)  
+
         train_orch.reset_data_index()
+        test_orch.reset_data_index()
+
 
 
 
