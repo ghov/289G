d61d2e79d1ea114afca93a84b3588b61c624941b
Nicholas Joodi <npjoodi@ucdavis.edu>
2018-03-09 04:02:12 -0800

    metrics for evaluation

---
+++ siamese/metrics.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import roc_curve, auc, precision_recall_curve, average_precision_score
+
+
+def get_metrics( Y, predictions):
+    print(Y)
+    fpr, tpr , _ = roc_curve(Y.ravel(), predictions.ravel())
+    roc_auc = auc(fpr, tpr)
+    precision, recall , _ = precision_recall_curve(Y.ravel(), predictions.ravel())
+    aucPR = auc(recall, precision)
+    average_precision = average_precision_score(Y.ravel(), predictions.ravel())  
+    return aucPR, average_precision, roc_auc
